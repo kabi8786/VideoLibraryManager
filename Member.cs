8fcd5ebf3874095e9d7ae2a64a901e2eb0c64741
Anna Nguyen <61668034+kabi8786@users.noreply.github.com>
2020-06-11 10:55:34 +1000

    Creates a member

---
+++ Member.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoLibraryManagement
+{
+    class Member
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string resAddress { get; set; }
+        public string userName { get; set; } //used to verify a member for login
+        public int password { get; set; }
+        public string contactNum { get; set; }
+
+        public List<Movie> borrowedMovies = new List<Movie>(10); //stores borrowed movies
+        
+        public Member(string fName, string lName)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+            //username is a concatenation of a member's last and first name
+            this.userName = lName + fName;
+        }
+    }
+}
